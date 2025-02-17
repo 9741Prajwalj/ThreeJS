0005391c957b9871406b2e12ce1483e5b7666677
9741Prajwalj <116446836+9741Prajwalj@users.noreply.github.com>
2025-02-17 09:09:08 +0530

    working on vite3d for importing Three JS library

---
+++ vite3d/src/main.js
@@ -0,0 +1,53 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+//initialize the scene
+const scene = new THREE.Scene();
+
+// add objects to the scene
+const cubeGeometry = new THREE.BoxGeometry(1,1,1)
+const cubeMaterial = new THREE.MeshBasicMaterial({color:"red"})
+
+const cubeMesh = new THREE.Mesh(
+  cubeGeometry,
+  cubeMaterial
+)
+scene.add(cubeMesh);
+
+cubeMesh.position.y = 1
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+// initialize the camera
+const camera = new THREE.PerspectiveCamera(75, 
+  window.innerWidth / window.innerHeight, 0.1, 200)
+
+// const aspectRatio = window.innerWidth / window.innerHeight
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio,1 * aspectRatio,1,-1,0.1,200)
+  // position
+  camera.position.z = 5
+
+  scene.add(camera)
+
+  // initailize the renderer
+  const canvas = document.querySelector('canvas.threejs')
+  const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true,
+  })
+  renderer.setSize(window.innerWidth,window.innerHeight)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio,2));
+
+// initailize the controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.autoRotate = false;
+
+const renderloop = () => {
+  controls.update()
+  renderer.render(scene,camera)
+  window.requestAnimationFrame(renderloop)
+}
+
+renderloop()
